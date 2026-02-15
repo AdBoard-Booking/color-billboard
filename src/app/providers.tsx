436e39a5af7153e4f7613f072bf465e61fdf16de
Ankur kushwaha <ankur.kus1@gmail.com>
2026-02-15 23:07:17 +0530

    base done

---
+++ src/app/providers.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
+import { useEffect } from "react";
+
+if (typeof window !== "undefined") {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY || "", {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
+    person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+    capture_pageview: false, // Disabling automatic pageview capture to avoid noise
+  });
+}
+
+export function CSPostHogProvider({ children }: { children: React.ReactNode }) {
+  return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
+}
